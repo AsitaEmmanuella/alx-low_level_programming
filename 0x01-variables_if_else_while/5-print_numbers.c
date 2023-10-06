8bab74fda0aa24efb629e38c0ca05294cedddb9a
AsitaEmmanuella <asitemmanuella19@gmail.com>
2023-10-06 16:49:42 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: when 0
+ */
+
+int main(void)
+{
+	int t;
+
+	for (t = 0 ; t < 10 ; t++)
+		printf("%d", t);
+	printf("\n");
+	return (0);
+}
